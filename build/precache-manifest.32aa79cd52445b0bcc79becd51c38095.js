6bec9a13ce18c13db328d4e8715515fc344439e4
ten16thomasg <tm.gray@hotmail.com>
2020-04-26 21:47:40 -0500

    Remove Build From gitignore

---
+++ build/precache-manifest.32aa79cd52445b0bcc79becd51c38095.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a14ca0b59f250ea1f5eb34eca7ad6e58",
+    "url": "/index.html"
+  },
+  {
+    "revision": "3e8dc3dcc3b69680b178",
+    "url": "/static/css/2.9dafe002.chunk.css"
+  },
+  {
+    "revision": "637bbd7fe6a5ecc4338a",
+    "url": "/static/css/main.760b5825.chunk.css"
+  },
+  {
+    "revision": "3e8dc3dcc3b69680b178",
+    "url": "/static/js/2.2681677b.chunk.js"
+  },
+  {
+    "revision": "637bbd7fe6a5ecc4338a",
+    "url": "/static/js/main.5e815142.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "d89ef1057a9af5f118c0db1a1a1a5bb5",
+    "url": "/static/media/Vmarine.d89ef105.jpg"
+  },
+  {
+    "revision": "94822dbf2b0e3c19951f72f314189593",
+    "url": "/static/media/myImage.94822dbf.png"
+  },
+  {
+    "revision": "d5e5ba246ae0ee0d85c82cd0ade0f6f6",
+    "url": "/static/media/todolist.d5e5ba24.png"
+  }
+]);
