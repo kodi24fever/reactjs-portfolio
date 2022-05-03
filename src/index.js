5ed3e8516f97e7ad011c5d3b419a2b3d2a5a910f
Frank <frankdavid93om@yahoo.com>
2022-05-02 23:40:34 -0400

    changing folder structures

--- src/index.js
+++ src/index.js
@@ -4,8 +4,8 @@ import './index.css';
 import * as serviceWorker from './serviceWorker';
 
 //adding my imports
-import Navbar from "./component/navbar.jsx";
-import Footer from "./component/footer.jsx";
+import Navbar from "./components/navbar.jsx";
+import Footer from "./components/footer.jsx";
 //import 'bootstrap/dist/css/bootstrap.css';
 import "./styles/index.scss";
 
