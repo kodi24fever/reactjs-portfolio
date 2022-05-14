f3ffa3d1e1f892c24e948a8a2f8fa5d936769496
Frank <frankdavid93om@yahoo.com>
2022-05-14 14:37:05 -0400

    remove unnecessary div for nav

--- src/components/navbar.js
+++ src/components/navbar.js
@@ -26,39 +26,37 @@ export default function Navbar() {
 	}, [])
 
 	return (
-		<div>
-			<nav
-				id="navbar"
-				className="navbar navbar-expand-lg navbar-light bg-light">
-				<div className="container">
-					<a className="home-style navbar-brand"
-						onClick={() => { 
-							window.scrollTo({
-								top: 0,
-								behavior: "smooth"
-							});
-						}}>
-						Back to Space
-					</a>
-					<button
-						className="navbar-toggler"
-						type="button"
-						data-bs-toggle="collapse"
-						data-bs-target="#navbarNavAltMarkup"
-						aria-controls="navbarNavAltMarkup"
-						aria-expanded="false"
-						aria-label="Toggle navigation">
-						<span className="navbar-toggler-icon" />
-					</button>
-					<div className="collapse navbar-collapse" id="navbarNavAltMarkup">
-						<div className="navbar-nav">
-							<a onClick={() => SmoothScrollTo("about-container")} className="btn-style nav-item nav-link">About</a>
-							<a onClick={() => SmoothScrollTo("my-projects")}className="btn-style nav-item nav-link">Projects</a>
-							<a onClick={() => SmoothScrollTo("my-contact")}className="btn-style nav-item nav-link">Contact</a>
-						</div>
+		<nav
+			id="navbar"
+			className="navbar navbar-expand-lg navbar-light bg-light">
+			<div className="container">
+				<a className="home-style navbar-brand"
+					onClick={() => { 
+						window.scrollTo({
+							top: 0,
+							behavior: "smooth"
+						});
+					}}>
+					Back to Space
+				</a>
+				<button
+					className="navbar-toggler"
+					type="button"
+					data-bs-toggle="collapse"
+					data-bs-target="#navbarNavAltMarkup"
+					aria-controls="navbarNavAltMarkup"
+					aria-expanded="false"
+					aria-label="Toggle navigation">
+					<span className="navbar-toggler-icon" />
+				</button>
+				<div className="collapse navbar-collapse" id="navbarNavAltMarkup">
+					<div className="navbar-nav">
+						<a onClick={() => SmoothScrollTo("about-container")} className="btn-style nav-item nav-link">About</a>
+						<a onClick={() => SmoothScrollTo("my-projects")}className="btn-style nav-item nav-link">Projects</a>
+						<a onClick={() => SmoothScrollTo("my-contact")}className="btn-style nav-item nav-link">Contact</a>
 					</div>
 				</div>
-			</nav>
-		</div>
+			</div>
+		</nav>
 	);
 }
